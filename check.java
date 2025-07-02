6aef36a075e206b834a2c14314235015126543d4
student-Pratik-ctrl <pratikdere103@gmail.com>
2025-07-02 09:39:13 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,12 @@
+package dayTwo;
+
+public class check {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b=2;
+		System.out.println(a/b);
+
+	}
+
+}
---
+++ conditionalStatement.java
@@ -0,0 +1,49 @@
+package dayTwo;
+
+public class conditionalStatement {
+
+	public static void main(String[] args) {
+//		int a=5;
+//		for(int i =1;i<=a;i++) {
+//			for(int space=1;space<=a-i;space++) {
+//				System.out.print(" ");
+//			}
+//			for(int star=1;star<=i;star++) {
+//				System.out.print("*");
+//			}
+//			System.out.println( );
+//		}
+//	
+//		int b = 5;
+//		for(int i =1;i<=b;i++) {
+//			for(int space=1;space<=b-i;space++) {
+//				System.out.print(" ");
+//			}
+//			for(int star =1;star<=2*i-1;star++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		int b = 5;
+//		for(int i =5;i>=1;i--) {
+//			for(int space=1;space<=b-i;space++) {
+//				System.out.print(" ");
+//			}
+//			for(int star =1;star<=2*i-1;star++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		int c = 5;
+		for(int i =1; i<=c;i++) {
+			for(int j =1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println( );
+		}
+	}
+		
+		
+}
+
